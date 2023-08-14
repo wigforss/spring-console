c2a10592e6aca1b98d786a3def4e06870f456355
Rikard Wigforss <Rikard.wigforss@kfbank.se>
2023-08-14 18:30:50 +0200

    Initial commit

---
+++ spring-console-react/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path:path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.css$/, 
+        use: [ 'style-loader', 'css-loader' ] 
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.?jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              '@babel/preset-env', 
+              '@babel/preset-react'
+            ]
+          }
+        }
+      },
+    ]
+  },
+  resolve: {
+    extensions: [".*", ".js", ".jsx"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+}
